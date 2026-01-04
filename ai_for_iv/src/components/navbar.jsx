210e84b5422ee5ea964364580599fade13829792
ahona04 <azkhanda@uci.edu>
2026-01-04 00:26:02 -0800

    navbar

--- ai_for_iv/src/components/navbar.jsx
+++ ai_for_iv/src/components/navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function navbar() {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='flex justify-between-center h-24 max-w-[1240px] mx-auto px-4 text-[#032163]'>
+        <h1 className='w-full text-3xl font-bold'>AI for IV</h1>
+        <ul className='flex'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Gallery</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+    </div>
   )
 }
 
-export default navbar
+export default Navbar;
